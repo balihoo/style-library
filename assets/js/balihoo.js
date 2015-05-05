7191bdd5961279636f29f89524ff18cbb952b55d
Andy Harl <aharl@balihoo.com>
2015-05-05 11:41:57 -0600

    INT-304: Updating Image Carousel

--- assets/js/balihoo.js
+++ assets/js/balihoo.js
@@ -20,21 +20,6 @@ $(function() { // Document Ready
 
 	///////////////////
 	/// Image Field
-	$('.bh--form-image .carousel .item').each(function(){
-		var next = $(this).next();
-		if (!next.length) {
-			next = $(this).siblings(':first');
-		}
-
-		next.children(':first-child').clone().appendTo($(this));
-
-		if (next.next().length>0) {
-			next.next().children(':first-child').clone().appendTo($(this)).addClass('rightest');
-		}
-		else {
-			$(this).siblings(':first').children(':first-child').clone().appendTo($(this));
-		}
-	});
 	$('.bh--form-image .carousel').carousel('pause');
 
 });
