3f7f5f221e5fc8b195522ea5863585a5b119e2b0
Andy Harl <aharl@balihoo.com>
2015-05-06 13:31:17 -0600

    INT-304: Starting build script

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function() {
+	// code coming
+});
+
+gulp.task('default', function() {
+	// place code for your default task here
+});
